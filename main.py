6885fe70a991b33107d440a0bc2dbedf0fdd16c2
Mohan S Acharya <mohan.acharya897@gmail.com>
2021-11-23 10:46:45 -0800

    Update main.py

--- main.py
+++ main.py
@@ -193,7 +193,10 @@ def main(args, ITE=0):
                 step = 0
             else:
                 original_initialization(mask, initial_state_dict)
-            optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=1e-4)
+            if 'cifar10' in args.dataset:
+                optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=0.5)
+            else:
+                optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=1e-4)
         print(f"\n--- Pruning Level [{ITE}:{_ite}/{ITERATION}]: ---")
 
         # Print the table of Nonzeros in each layer
